dceba11b15a1f0174d4062caedbb450bd8bf9898
marian1234560 <marianyelipssabeltran@gmail.com>
2021-05-07 22:12:10 -0500

    Tabla de Bootstrap en JavaScript

---
+++ Js/Tabla.js
@@ -0,0 +1,87 @@
+for (let i = 0; i <= 0; i++) {
+  let card = `<div class="m-3 card" style="width: 50rem;">
+
+<div>
+<table class="table table-striped">
+<thead class="thead-dark">
+  <tr>
+    <th scope="col">#</th>
+    <th scope="col">First</th>
+    <th scope="col">Last</th>
+    <th scope="col">Handle</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <th scope="row">2</th>
+    <td>Mark</td>
+    <td>Otto</td>
+    <td>@mdo</td>
+  </tr>
+  <tr>
+    <th scope="row">4</th>
+    <td>Jacob</td>
+    <td>Thornton</td>
+    <td>@fat</td>
+  </tr>
+  <tr>
+    <th scope="row">6</th>
+    <td>Larry</td>
+    <td>the Bird</td>
+    <td>@twitter</td>
+  </tr>
+  <tr>
+    <th scope="row">8</th>
+    <td>Mero</td>
+    <td>perz</td>
+    <td>@jskjsk</td>
+  </tr>
+  <tr>
+    <th scope="row">10</th>
+    <td>Jacob</td>
+    <td>Thornton</td>
+    <td>@fabio</td>
+  </tr>
+  <tr>
+    <th scope="row">12</th>
+    <td>Larry</td>
+    <td>the Bird</td>
+    <td>@milena</td>
+  </tr>
+  <tr>
+    <th scope="row">12</th>
+    <td>Larry</td>
+    <td>aguilar</td>
+    <td>@ivan</td>
+  </tr>
+    <tr>
+    <th scope="row">14</th>
+    <td>Larry</td>
+    <td>teodorO</td>
+    <td>@juan</td>
+  </tr>
+  <tr>
+    <th scope="row">16</th>
+    <td>LaLA</td>
+    <td>Muñoz</td>
+    <td>@perid</td>
+  </tr>
+  <tr>
+    <th scope="row">18</th>
+    <td>Lulu</td>
+    <td>tBird</td>
+    <td>@sandrita</td>
+  </tr>
+  <tr>
+    <th scope="row">20</th>
+    <td>paula</td>
+    <td>pinzon</td>
+    <td>@patriciaGIm</td>
+  </tr>
+</tbody>
+</table>
+</div>
+</div>`;
+
+  document.getElementById("contenedor-tabla").innerHTML += card;
+}
