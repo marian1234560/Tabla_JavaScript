492b156882e6e7b1038f8bda8d2e5b154ce13b15
marian1234560 <marianyelipssabeltran@gmail.com>
2021-05-12 20:33:29 -0500

    Correccion de la tabal

--- Js/Tabla.js
+++ Js/Tabla.js
@@ -1,87 +1,22 @@
-for (let i = 0; i <= 0; i++) {
-  let card = `<div class="m-3 card" style="width: 50rem;">
+function imprimir() {
+  document.getElementById("print-table").innerHTML = "";
 
-<div>
-<table class="table table-striped">
-<thead class="thead-dark">
-  <tr>
-    <th scope="col">#</th>
-    <th scope="col">First</th>
-    <th scope="col">Last</th>
-    <th scope="col">Handle</th>
-  </tr>
-</thead>
-<tbody>
-  <tr>
-    <th scope="row">2</th>
-    <td>Mark</td>
-    <td>Otto</td>
-    <td>@mdo</td>
-  </tr>
-  <tr>
-    <th scope="row">4</th>
-    <td>Jacob</td>
-    <td>Thornton</td>
-    <td>@fat</td>
-  </tr>
-  <tr>
-    <th scope="row">6</th>
-    <td>Larry</td>
-    <td>the Bird</td>
-    <td>@twitter</td>
-  </tr>
-  <tr>
-    <th scope="row">8</th>
-    <td>Mero</td>
-    <td>perz</td>
-    <td>@jskjsk</td>
-  </tr>
-  <tr>
-    <th scope="row">10</th>
-    <td>Jacob</td>
-    <td>Thornton</td>
-    <td>@fabio</td>
-  </tr>
-  <tr>
-    <th scope="row">12</th>
-    <td>Larry</td>
-    <td>the Bird</td>
-    <td>@milena</td>
-  </tr>
-  <tr>
-    <th scope="row">12</th>
-    <td>Larry</td>
-    <td>aguilar</td>
-    <td>@ivan</td>
-  </tr>
-    <tr>
-    <th scope="row">14</th>
-    <td>Larry</td>
-    <td>teodorO</td>
-    <td>@juan</td>
-  </tr>
-  <tr>
-    <th scope="row">16</th>
-    <td>LaLA</td>
-    <td>Muñoz</td>
-    <td>@perid</td>
-  </tr>
-  <tr>
-    <th scope="row">18</th>
-    <td>Lulu</td>
-    <td>tBird</td>
-    <td>@sandrita</td>
-  </tr>
-  <tr>
-    <th scope="row">20</th>
-    <td>paula</td>
-    <td>pinzon</td>
-    <td>@patriciaGIm</td>
-  </tr>
-</tbody>
-</table>
-</div>
-</div>`;
+  let i = 0;
+  while (i <= 20) {
+    let bodyTabla = `<tr>
+         <th class="table-secondary" scope="row">${i}</th>
+         <td class="table-info"hola${i}</td>
+         <td class="table-info">8749${i}</td>
+         <td class="table-info">@hola${i}</td>
+         <td class="table-info">322452736${i}</td>
+       </tr>`;
 
-  document.getElementById("contenedor-tabla").innerHTML += card;
+    console.log(i);
+
+    document.getElementById("print-table").innerHTML += bodyTabla;
+
+    /* De 2 en 2*/
+    i ++;
+    i ++;
+  }
 }
